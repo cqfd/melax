e6063005b38a21a42adf4108be12d9aaa3beef22
cqfd <alan.m.odonnell@gmail.com>
2023-09-16 12:51:01 +0300

    test error helpers on Input blocks

--- tests/test_modals.py
+++ tests/test_modals.py
@@ -1,7 +1,7 @@
 from unittest import mock
 from unittest.mock import Mock
 
-from melax.modals import Button
+from melax.modals import Button, Errors, Input, NumberInput, Ok
 
 
 def test_button_callbacks() -> None:
@@ -26,3 +26,16 @@ def test_button_callbacks_interspersed_with_maps() -> None:
     b._on_action({})
 
     cb.assert_has_calls([mock.call("ok"), mock.call(len("ok")), mock.call(-len("ok"))])
+
+
+def test_error_helpers() -> None:
+    i = Input("Enter a number", NumberInput(is_decimal_allowed=False)).error_if(
+        lambda x: x == 7, "7 is unlucky"
+    )
+    i._block_id = "block_id"
+
+    p = i._parse({"block_id": {"NumberInput": {"value": "7"}}})
+    assert p == Errors({"block_id": "7 is unlucky"})
+
+    p2 = i._parse({"block_id": {"NumberInput": {"value": "123"}}})
+    assert p2 == Ok(123)
