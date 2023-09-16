76a59e1a3d4335f69c79b774e339f4f391e4f7e9
cqfd <alan.m.odonnell@gmail.com>
2023-09-16 23:04:26 +0300

    more tests

--- tests/test_modals.py
+++ tests/test_modals.py
@@ -11,11 +11,33 @@ from melax.modals import (
     Input,
     NumberInput,
     Ok,
+    Option,
+    PlainTextInput,
     Section,
+    Select,
 )
 
 
-def test_sections() -> None:
+def test_input_blocks() -> None:
+    cb = Mock()
+
+    class Form(Builder):
+        name = Input("Name:", PlainTextInput().on_enter_pressed(cb))
+
+    p = Form._parse({})
+    assert p is None
+
+    p = Form._parse({"name": {"PlainTextInput": {"value": "Foo"}}})
+
+    assert isinstance(p, Ok)
+    assert isinstance(p.value, Form)
+    assert p.value.name == "Foo"
+
+    Form._on_block_action("name", "PlainTextInput", {"value": "Foo"})
+    cb.assert_called_with("Foo")
+
+
+def test_section_blocks() -> None:
     class Form(Builder):
         s = Section("Hello!")
 
@@ -23,7 +45,6 @@ def test_sections() -> None:
     assert isinstance(p, Ok)
     assert isinstance(p.value, Form)
     assert p.value.s is None
-
 
     class Form2(Builder):
         s = Section("Hello", accessory=DatePicker())
@@ -34,30 +55,19 @@ def test_sections() -> None:
     assert isinstance(p2.value, Form2)
     assert p2.value.s is None
 
-    p3 = Form2._parse({
-        "s": {
-          "DatePicker": {
-            "type": "datepicker",
-            "selected_date": None
-          }
-        }
-      })
+    p2 = Form2._parse(
+        {"s": {"DatePicker": {"type": "datepicker", "selected_date": None}}}
+    )
 
-    assert isinstance(p3, Ok)
-    assert isinstance(p3.value, Form2)
+    assert isinstance(p2, Ok)
+    assert isinstance(p2.value, Form2)
 
-    p4 = Form2._parse({
-        "s": {
-          "DatePicker": {
-            "type": "datepicker",
-            "selected_date": "2023-09-02"
-          }
-        }
-    })
-    assert isinstance(p4, Ok)
-    assert isinstance(p4.value, Form2)
-    assert p4.value.s == datetime.date.fromisoformat("2023-09-02")
-
+    p2 = Form2._parse(
+        {"s": {"DatePicker": {"type": "datepicker", "selected_date": "2023-09-02"}}}
+    )
+    assert isinstance(p2, Ok)
+    assert isinstance(p2.value, Form2)
+    assert p2.value.s == datetime.date.fromisoformat("2023-09-02")
 
 
 def test_button_callbacks() -> None:
@@ -85,16 +95,17 @@ def test_button_callbacks_interspersed_with_maps() -> None:
 
 
 def test_error_helpers() -> None:
-    i = Input("Enter a number", NumberInput(is_decimal_allowed=False)).error_if(
-        lambda x: x == 7, "7 is unlucky"
-    )
-    i._block_id = "block_id"
+    class Form(Builder):
+        i = Input("Enter a number", NumberInput(is_decimal_allowed=False)).error_if(
+            lambda x: x == 7, "7 is unlucky"
+        )
 
-    p = i._parse({"block_id": {"NumberInput": {"value": "7"}}})
-    assert p == Errors({"block_id": "7 is unlucky"})
+    p = Form._parse({"i": {"NumberInput": {"value": "7"}}})
+    assert p == Errors({"i": "7 is unlucky"})
 
-    p2 = i._parse({"block_id": {"NumberInput": {"value": "123"}}})
-    assert p2 == Ok(123)
+    p2 = Form._parse({"i": {"NumberInput": {"value": "123"}}})
+    assert isinstance(p2, Ok)
+    assert p2.value.i == 123
 
 
 def test_actions_blocks() -> None:
@@ -102,12 +113,12 @@ def test_actions_blocks() -> None:
     nay = Mock()
     dob = Mock()
 
-    yay_or_nay = Actions(
-        yay=Button("Yay", value="yay", style="primary").on_pressed(yay),
-        nay=Button("Nay", value="nay", style="danger").on_pressed(nay),
-        dob=DatePicker().on_picked(dob),
-    )
-    yay_or_nay._block_id = "yay_or_nay"
+    class Form(Builder):
+        yay_or_nay = Actions(
+            yay=Button("Yay", value="yay", style="primary").on_pressed(yay),
+            nay=Button("Nay", value="nay", style="danger").on_pressed(nay),
+            dob=DatePicker().on_picked(dob),
+        )
 
     yay_action = {
         "action_id": "yay",
@@ -119,7 +130,7 @@ def test_actions_blocks() -> None:
         "action_ts": "1694859764.558458",
     }
 
-    yay_or_nay._on_action("yay", yay_action)
+    Form._on_block_action("yay_or_nay", "yay", yay_action)
     yay.assert_called_with("yay")
 
     today = datetime.date.today()
@@ -133,14 +144,55 @@ def test_actions_blocks() -> None:
         "action_ts": "1694860134.517951",
     }
 
-    yay_or_nay._on_action("dob", date_picker_action)
+    Form._on_block_action("yay_or_nay", "dob", date_picker_action)
     dob.assert_called_with(today)
 
     submit_payload = {
         "yay_or_nay": {"dob": {"type": "datepicker", "selected_date": None}}
     }
 
-    p = yay_or_nay._parse(submit_payload)
+    p = Form._parse(submit_payload)
     assert isinstance(p, Ok)
+    assert p.value.yay_or_nay == {"yay": "yay", "nay": "nay", "dob": None}
 
-    assert p.value == {"yay": "yay", "nay": "nay", "dob": None}
+
+def test_select_elements() -> None:
+    chocolate = Option("Chocolate", "chocolate")
+    vanilla = Option("Vanilla", "vanilla")
+    static_options = [chocolate, vanilla]
+
+    class Static(Builder):
+        fav_ice_cream = Input("Favorite ice cream", Select(options=static_options))
+
+    p = Static._parse(
+        {
+            "fav_ice_cream": {
+                "Select": {
+                    "type": "static_select",
+                    "selected_option": {
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Chocolate",
+                            "emoji": True,
+                        },
+                        "value": "chocolate",
+                    },
+                }
+            }
+        }
+    )
+    assert isinstance(p, Ok)
+    assert p.value.fav_ice_cream
+
+    def external_options(query: str) -> list[Option]:
+        return [
+            o for o in static_options if o.text.lower().startswith(query)
+        ]
+    mock = Mock(side_effect=external_options)
+
+    class Dynamic(Builder):
+        fav_ice_cream = Input("Favorite ice cream", Select(options=mock))
+
+    opts = Dynamic._on_block_options("fav_ice_cream", "Select", "choco")
+    mock.assert_called_with("choco")
+    assert opts == [chocolate]
